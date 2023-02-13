824e213cdb881d9fa4a04a915ef8738b40760e8a
Nithin-Varma <mnithin1422@gmail.com>
2023-02-13 16:57:09 +0530

    signup page

--- frontend/pages/signup.tsx
+++ frontend/pages/signup.tsx
@@ -3,6 +3,10 @@ import { User } from '@/types/user';
 import {
   Box,
   Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
   Input,
   InputGroup,
   InputLeftAddon,
@@ -16,93 +20,105 @@ import { useState } from 'react';
 import { BLOODGROUPS } from 'src/constants';
 function index() {
   const [user, setUser] = useState<User>({} as User);
+ 
   return (
-    <PageLayout title='Sign Up' description='Sign up to med chain'>
-      <Stack
-        width={'100%'}
-        spacing={8}
-        marginTop='12'
-        marginInline={'auto'}
-        py={12}
-        align='center'
-        h='100vh'
-        minW={'100%'}
+    <PageLayout title='Sign Up' description='Sign up page for medchain'>
+      <>
+<Flex
+      
+      minW={"100%"}
+      marginTop={'100'}
+      align={'center'}
+      justify={'center'}
       >
-        <Box width={'70%'} backgroundColor='blue.300'>
-          <Text
-            textAlign={'center'}
-            marginY='6'
-            fontSize={'3xl'}
-            fontWeight='bold'
-          >
-            User Bio
-          </Text>
-          <VStack margin={'8%'} spacing={8}>
-            <InputGroup>
-              <InputLeftAddon children='Full Name' />
-              <Input
-                required
-                type='text'
-                value={user.fullName}
-                onChange={(e) => {
-                  setUser({
-                    ...user,
-                    fullName: e.target.value,
-                  });
-                }}
-                placeholder='phone number'
-              />
-            </InputGroup>
-            <InputGroup>
-              <InputLeftAddon children='Age' />
-              <Input
-                value={user.age}
-                onChange={(e) => {
-                  9;
-                  setUser({
-                    ...user,
-                    age: parseInt(e.target.value),
-                  });
-                }}
-                required
-                type='number'
-                placeholder='Age'
-              />
-            </InputGroup>
-            <SelectDefault required placeholder='Blood Group'>
+      <Stack
+        spacing={8}
+        w={'full'}
+        maxW={'md'}
+        boxShadow={'#69D3FA 0px 0px 0px 2px, #69D3FA 0px 2px 3px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset'}
+        
+        // bg={useColorModeValue('white', 'gray.700')}
+        rounded={'xl'}
+        
+        p={6}
+        my={12}>
+        <Heading lineHeight={1.1} fontSize={{ base:'4xl', sm: '3xl' }}>
+          Enter Your Details Here
+        </Heading>
+        <FormControl id="userName" isRequired>
+          <FormLabel>User name</FormLabel>
+          <Input
+            placeholder="Full Name"
+            _placeholder={{ color: 'gray.500' }}
+            type="text"
+            value={user.fullName}
+            onChange={(e) => {
+              setUser({
+                ...user,
+                fullName: e.target.value,
+              });
+            }}
+          />
+        </FormControl>
+        <FormControl id="age" isRequired >
+          <FormLabel fontSize={{ base:'4xl', sm: 'xl' }}>Age</FormLabel>
+          <Input
+           value={user.age}
+           onChange={(e) => {
+             setUser({
+               ...user,
+               age:parseInt(e.target.value),
+             });
+           }}
+            placeholder="Age"
+            _placeholder={{ color: 'gray.500' }}
+            type="number"
+            fontSize={{ base:'4xl', sm: 'xl' }}
+            blockSize={{base:'20', sm:'10'}}
+          />
+        </FormControl>
+        <FormControl id="blood group" isRequired>
+          <FormLabel>Blood Group</FormLabel>
+          <SelectDefault required placeholder='Blood Group'>
               {BLOODGROUPS.map((group) => (
                 <option value={group}>{group}</option>
               ))}
             </SelectDefault>
-            <Text textAlign={'left'} width='100%' mb='8px'></Text>
-            <InputGroup>
-              <InputLeftAddon children='Allergies' />
-              <Input
-                value={user.allergies}
-                onChange={(e) => {
-                  setUser({
-                    ...user,
-                    allergies: e.target.value,
-                  });
-                }}
-                type='text'
-                placeholder='Sperate the different Allergies using comma'
-              />
-            </InputGroup>
-            <InputGroup>
-              <InputLeftAddon children='Medications' />
-              <Input
-                value={user.medication}
-                onChange={(e) => {
-                  setUser({
-                    ...user,
-                    medication: e.target.value,
-                  });
-                }}
-                type='text'
-                placeholder='   Sperate the different medication using comma'
-              />
-            </InputGroup>
+        </FormControl>
+        <FormControl id='allergies' isRequired> 
+            <FormLabel>Allergies</FormLabel>
+            <Input 
+             value={user.allergies}
+             onChange={(e) => {
+               setUser({
+                 ...user,
+                 allergies: e.target.value,
+               });
+             }}
+            placeholder="In case of Nothing write NIL"
+            _placeholder={{
+                color:'gray.500'
+            }}
+            />
+        </FormControl>
+        <FormControl id='medications' isRequired> 
+            <FormLabel>Medications</FormLabel>
+            <Input 
+             value={user.medication}
+             onChange={(e) => {
+               setUser({
+                 ...user,
+                 medication: e.target.value,
+               });
+             }}
+            placeholder="In case of Nothing write NIL"
+            _placeholder={{
+                color:'gray.500'
+            }}
+            />
+        </FormControl>
+        <FormControl id="about">
+            <FormLabel>Recent Medical Document</FormLabel>
             <Textarea
               placeholder=' About Your self'
               size='sm'
@@ -117,10 +133,30 @@ function index() {
               colorScheme={'blackAlpha'}
               variant={'filled'}
             />
-            <Button>Submit</Button>
-          </VStack>
-        </Box>
+        </FormControl>
+        <Stack spacing={6} direction={['column', 'row']}>
+          <Button
+            bg={'red.400'}
+            color={'white'}
+            w="full"
+            _hover={{
+              bg: 'red.500',
+            }}>
+            Reset
+          </Button>
+          <Button
+            bg={'blue.400'}
+            color={'white'}
+            w="full"
+            _hover={{
+              bg: 'blue.500',
+            }}>
+            Submit
+          </Button>
+        </Stack>
       </Stack>
+    </Flex>
+        </>
     </PageLayout>
   );
 }
--- frontend/src/components/user_input-page/User_input.tsx
+++ frontend/src/components/user_input-page/User_input.tsx
@@ -1,10 +1,146 @@
+import { LinkIcon, SmallCloseIcon } from "@chakra-ui/icons";
 import {
-    Text,
-} from '@chakra-ui/react'
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Stack,
+  Text,
+  Image,
+  useColorModeValue as mode,
+  VStack,
+  Center,
+  HStack,
+  Card,
+  CardFooter,
+  CardBody,
+  CardHeader,
+  extendTheme,
+  Divider,
+  Avatar,
+  AvatarBadge,
+  FormControl,
+  FormLabel,
+  IconButton,
+  Input,
+  useColorModeValue
+} from "@chakra-ui/react";
+
+import Head from "next/head";
+import { useState } from "react";
 
 function UserInput() {
+
+    const [UserName, SetUserName] = useState("");
+    const [Age, SetAge] = useState("");
+    const [Blood, SetBlood] = useState("");
+    const [Allergies, SetAllergies] = useState("");
+    const [Medications, SetMedications] = useState("");
+    const [About, SetAbout] = useState("");
+
     return (
-        <Text>Nithin</Text>
+
+<>
+    <Flex
+      minH={'100vh'}
+      minW={"1000%"}
+      marginTop={'100'}
+      align={'center'}
+      justify={'center'}
+      >
+      <Stack
+        spacing={8}
+        w={'full'}
+        maxW={'md'}
+       
+        // bg={useColorModeValue('white', 'gray.700')}
+        rounded={'xl'}
+        boxShadow={'lg'}
+        p={6}
+        my={12}>
+        <Heading lineHeight={1.1} fontSize={{ base:'4xl', sm: '3xl' }}>
+          Enter Your Details Here
+        </Heading>
+        <FormControl id="userName" isRequired>
+          <FormLabel>User name</FormLabel>
+          <Input
+            placeholder="UserName"
+            _placeholder={{ color: 'gray.500' }}
+            type="text"
+          />
+        </FormControl>
+        <FormControl id="age" isRequired >
+          <FormLabel fontSize={{ base:'4xl', sm: 'xl' }}>Age</FormLabel>
+          <Input
+            placeholder="Age"
+            _placeholder={{ color: 'gray.500' }}
+            type="number"
+            fontSize={{ base:'4xl', sm: 'xl' }}
+            blockSize={{base:'20', sm:'10'}}
+          />
+        </FormControl>
+        <FormControl id="blood group" isRequired>
+          <FormLabel>Blood Group</FormLabel>
+          <Input
+            placeholder="Blood Group"
+            _placeholder={{ 
+                color: 'gray.500'
+             }}
+            type="text"
+          />
+        </FormControl>
+        <FormControl id='allergies' isRequired> 
+            <FormLabel>Allergies</FormLabel>
+            <Input 
+            placeholder="In case of Nothing write NIL"
+            _placeholder={{
+                color:'gray.500'
+            }}
+            />
+        </FormControl>
+        <FormControl id='medications' isRequired> 
+            <FormLabel>Medications</FormLabel>
+            <Input 
+            placeholder="In case of Nothing write NIL"
+            _placeholder={{
+                color:'gray.500'
+            }}
+            />
+        </FormControl>
+        <FormControl id="document" isRequired>
+            <FormLabel>Recent Medical Document</FormLabel>
+            <Input
+            placeholder="upload here"
+            type='file'
+            // htmlSize={"100"}
+            size={'lg'}
+            // width='auto'
+            />
+        </FormControl>
+        <Stack spacing={6} direction={['column', 'row']}>
+          <Button
+            bg={'red.400'}
+            color={'white'}
+            w="full"
+            _hover={{
+              bg: 'red.500',
+            }}>
+            Reset
+          </Button>
+          <Button
+            bg={'blue.400'}
+            color={'white'}
+            w="full"
+            _hover={{
+              bg: 'blue.500',
+            }}>
+            Submit
+          </Button>
+        </Stack>
+      </Stack>
+    </Flex>
+        </>
+        
     )
 
 }
