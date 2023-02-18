90c0f5f094fb7f73c48ea37bdda71ca83926449b
dipanshuhappy <dipanshuhappy@gmail.com>
2023-02-18 20:45:47 +0530

    remove wagmi config

--- frontend/wagmi.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from '@wagmi/cli';
-import { react } from '@wagmi/cli/plugins';
-export default defineConfig({
-  out: 'src/web3Hooks/hooks.ts',
-  plugins: [react({})],
-});
